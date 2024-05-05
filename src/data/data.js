48d4077d407381b07ebab8d4a8f0fba409f8c844
unknown <baburaja3421@gmail.com>
2024-05-06 03:49:19 +0545

    adding project todo

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,6 @@
 import movieApp from "../assets/images/React_Movie_App.png";
 import teslaReact from "../assets/images/Tesla_Clone_React.png";
+import reactTodo from "../assets/images/reactTodo.png";
 export const data = [
   {
     id: 1,
@@ -14,5 +15,12 @@ export const data = [
     img: movieApp,
     github: "https://github.com/Bipin-3421/react_movie_app.git ",
     live: "https://react-movie-app-blond-ten.vercel.app/",
+  },
+  {
+    id: 3,
+    name: "React Todo App",
+    img: reactTodo,
+    github: "https://github.com/Bipin-3421/React-Todo",
+    live: "https://react-redux-todo-48f08.web.app/",
   },
 ];
